3d60f64dbeb8050a96060c08838fb71226d8fb96
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-16 01:14:36 +0600

    started

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Stack } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Auth from "../layouts/Auth";
+import Error from "../pages/Error"; // Ensure this exists or create it.
+import { Route, Routes } from "react-router";
+import NavbarComponent from "../components/Navbar"; 
+const Main = () => {
+  return (
+    <div>
+      {/* Navbar Component */}
+      <NavbarComponent />
+
+      {/* Routes Component */}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="auth" element={<Auth />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Main;
